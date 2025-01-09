a7e02e4fc1960cf11b10fcf07b3fd156446ebdba
evgeniy <evgeniy.chernyak1982@gmail.com>
2025-01-09 12:57:32 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,58 +1,54 @@
--- Drop the existing database if it exists
-DROP DATABASE IF EXISTS ShopDB;
+-- Create database and tables
 
--- Create the new normalized database
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
--- Create Countries table
 CREATE TABLE Countries (
-    ID INT PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL
+    ID INT AUTO_INCREMENT,
+    Name VARCHAR(50),
+    PRIMARY KEY (ID)
 );
 
--- Create Products table
-CREATE TABLE Products (
-    ID INT PRIMARY KEY,
-    ProductName VARCHAR(50) NOT NULL
-);
-
--- Create Warehouses table
 CREATE TABLE Warehouses (
-    ID INT PRIMARY KEY,
-    WarehouseName VARCHAR(50) NOT NULL,
-    WarehouseAddress VARCHAR(50) NOT NULL,
+    ID INT AUTO_INCREMENT,
+    Name VARCHAR(50),
+    Address VARCHAR(50),
     CountryID INT,
-    FOREIGN KEY (CountryID) REFERENCES Countries(ID)
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID)  ON DELETE CASCADE,
+    PRIMARY KEY (ID)
 );
 
--- Create ProductWarehouseInventory table (many-to-many relationship between Products and Warehouses)
-CREATE TABLE ProductWarehouseInventory (
+CREATE TABLE Products (
+    ID INT AUTO_INCREMENT,
+    Name VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE ProductInventory (
+    ID INT AUTO_INCREMENT,
     ProductID INT,
+    WarehouseAmount INT,
     WarehouseID INT,
-    WarehouseAmount INT NOT NULL,
-    PRIMARY KEY (ProductID, WarehouseID),
-    FOREIGN KEY (ProductID) REFERENCES Products(ID),
-    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID)
+	FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE CASCADE,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE CASCADE,
+    PRIMARY KEY (ID)
 );
 
 -- Populate test data
 
--- Insert data into Countries table
-INSERT INTO Countries (ID, Name) VALUES
-    (1, 'Country1'),
-    (2, 'Country2');
+INSERT INTO Countries (Name)
+VALUES ('Country1'), ('Country2');
 
--- Insert data into Products table
-INSERT INTO Products (ID, ProductName) VALUES
-    (1, 'AwesomeProduct');
+INSERT INTO Warehouses (Name, Address, CountryID)
+VALUES
+    ('Warehouse-1', 'City-1, Street-1', 1),
+    ('Warehouse-2', 'City-2, Street-2', 2);
 
--- Insert data into Warehouses table
-INSERT INTO Warehouses (ID, WarehouseName, WarehouseAddress, CountryID) VALUES
-    (1, 'Warehouse-1', 'City-1, Street-1', 1),
-    (2, 'Warehouse-2', 'City-2, Street-2', 2);
+INSERT INTO Products (Name)
+VALUES
+    ('AwesomeProduct');
 
--- Insert data into ProductWarehouseInventory table
-INSERT INTO ProductWarehouseInventory (ProductID, WarehouseID, WarehouseAmount) VALUES
-    (1, 1, 2),
-    (1, 2, 5);
+INSERT INTO ProductInventory (ProductID, WarehouseAmount, WarehouseID)
+VALUES
+    (1, 2, 1),
+    (1, 5, 2);
